222af3e9f6426ca6699184dd0b53929e2dd7c8a6
JulietK <julietgisemba000@>
2020-07-26 11:59:49 +0300

    styled the css

--- scripts.js
+++ scripts.js
@@ -1,6 +1,16 @@
-// $(document).ready(function() {
-//     $("p").click(function() {
-//       $(".walrus-showing").toggle();
-//       $(".walrus-hidden").toggle();
-//     });
-//   })
+// //    what we do section
+$('.main').click(function () {
+    $(this).children('.hide').slideToggle(1200);
+    $(this).children('.show').toggle(1500);
+  });
+  // giving feedback to the customer
+  $("button").click(function (omonge) {
+    var client = document.getElementById('nameDetail').value;
+    alert(' Dear ' + ' esteemed ' + client + ' we have received your message successful. ' + ' Feel free to reach out to us anytime');
+    omonge.preventDefault();
+  });
+  
+  //portifolio section 
+  $(".col-mod-3").hover(function () {
+    $(this).children(".card-1").fadeToggle(1000, "linear");
+  });
